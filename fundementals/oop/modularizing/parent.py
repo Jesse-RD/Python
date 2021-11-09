6f43c7047f3407bac81d614a48ae5a943226b695
Jesse Duvall <jesserduvall@outlook.com>
2021-11-08 18:46:33 -0800

    initial commit

---
+++ fundementals/oop/modularizing/parent.py
@@ -0,0 +1,19 @@
+local_val = "magical unicorns"
+def square(x):
+    return x * x
+class User:
+    def __init__(self, name):
+        self.name = name
+    def say_hello(self):
+        return "hello"
+
+if __name__ == "__main__":
+    print("the file is being executed directly")
+else:
+    print("the file is being executed because it is imported by another file. the file is called: ", __name__)
+
+print(square(5))
+user = User("Anna")
+print(user.name)
+print(user.say_hello())
+print(__name__)
