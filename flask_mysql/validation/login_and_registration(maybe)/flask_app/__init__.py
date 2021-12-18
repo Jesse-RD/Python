2580f8c8b33acd509f1f5e13c2dfe65f77d0682b
Jesse Duvall <jesserduvall@outlook.com>
2021-12-17 20:58:32 -0800

    delete duplicate

--- flask_mysql/validation/login_and_registration(maybe)/flask_app/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-app.secret_key = "this is a secret be quiet"
--- flask_mysql/validation/login_and_registration(maybe)/flask_app/controllers/users.py
+++
@@ -1,4 +0,0 @@
-from flask_app import app
-from flask import render_template, redirect, request
-from flask_app.models.user import Users
-
